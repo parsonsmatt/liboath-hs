caa4681a96aab71e20a2ac0f411951fa27895aa5
Matt Parsons <parsonsmatt@gmail.com>
2017-10-25 10:42:01 -0600

    nice, same output

--- src/LibOath/Internal.c
+++ src/LibOath/Internal.c
@@ -15,3 +15,38 @@ const char * inline_c_LibOath_Internal_2_09ad50aa4440ab320ce845ed83cbd60f5df7808
 return ( oath_check_version(minVersion_inline_c_0) );
 }
 
+
+const char * inline_c_LibOath_Internal_3_ffc6a8a5085d32853b6c7f73038d1c1e3ea3fbb2(int err_inline_c_0) {
+return ( oath_strerror(err_inline_c_0) );
+}
+
+
+int inline_c_LibOath_Internal_4_2e7b1f4bb1c11117a98ae1f9462b140e1e6a32e1(const char * in__inline_c_0, size_t inlen_inline_c_1, char ** out_inline_c_2, size_t * outlen_inline_c_3) {
+return ( 
+        oath_base32_decode(in__inline_c_0, inlen_inline_c_1, out_inline_c_2, outlen_inline_c_3) 
+    );
+}
+
+
+int inline_c_LibOath_Internal_5_a2e36bd27888e723adba5735702f90927e00ff28(const char * in__inline_c_0, size_t inlen_inline_c_1, char ** out_inline_c_2, size_t * outlen_inline_c_3) {
+return ( 
+        oath_base32_encode(in__inline_c_0, inlen_inline_c_1, out_inline_c_2, outlen_inline_c_3) 
+    );
+}
+
+
+int inline_c_LibOath_Internal_6_169965319a18591060847b551fdf97ead7a1885f(const char * secret_inline_c_0, size_t secretLen_inline_c_1, time_t now_inline_c_2, unsigned timeStepSize_inline_c_3, time_t startOffset_inline_c_4, unsigned digits_inline_c_5, int flags_inline_c_6, char * outputOtp_inline_c_7) {
+return (
+        oath_totp_generate2(
+            secret_inline_c_0, 
+            secretLen_inline_c_1,
+            now_inline_c_2,
+            timeStepSize_inline_c_3,
+            startOffset_inline_c_4,
+            digits_inline_c_5,
+            flags_inline_c_6,
+            outputOtp_inline_c_7
+        ) 
+    );
+}
+
--- src/LibOath/Internal.hs
+++ src/LibOath/Internal.hs
@@ -12,13 +12,20 @@ Stability   : experimental
 Portability : POSIX
 
 This module contains the C functions called via "inline-c". Import these to use at your own risk.
+
+The functions are undocumented here. They are mirrored from <http://www.nongnu.org/oath-toolkit/liboath-api/liboath-oath.html the library documentation>.
+
 -}
 module LibOath.Internal where
 
 import qualified Language.C.Inline as C
 import qualified Foreign.C.String as C
+import Foreign.Ptr (Ptr)
 
 C.include "<oath.h>"
+
+_OATH_OK :: C.CInt
+_OATH_OK = 0
 
 oath_init :: IO C.CInt
 oath_init = [C.exp|int { oath_init() }|]
@@ -30,4 +37,33 @@ oath_check_version :: C.CString -> IO C.CString
 oath_check_version minVersion = 
     [C.exp|const char * { oath_check_version($(const char * minVersion)) }|]
 
+oath_strerror :: C.CInt -> IO C.CString
+oath_strerror err = [C.exp|const char * { oath_strerror($(int err)) }|]
 
+oath_base32_decode :: C.CString -> C.CSize -> Ptr C.CString -> Ptr C.CSize -> IO C.CInt
+oath_base32_decode in_ inlen out outlen =
+    [C.exp|int { 
+        oath_base32_decode($(const char * in_), $(size_t inlen), $(char ** out), $(size_t * outlen)) 
+    }|]
+
+oath_base32_encode :: C.CString -> C.CSize -> Ptr C.CString -> Ptr C.CSize -> IO C.CInt
+oath_base32_encode in_ inlen out outlen =
+    [C.exp|int { 
+        oath_base32_encode($(const char * in_), $(size_t inlen), $(char ** out), $(size_t * outlen)) 
+    }|]
+
+oath_totp_generate2 
+    :: C.CString -> C.CSize -> C.CTime -> C.CUInt -> C.CTime -> C.CUInt -> C.CInt -> C.CString -> IO C.CInt
+oath_totp_generate2 secret secretLen now timeStepSize startOffset digits flags outputOtp =
+    [C.exp|int {
+        oath_totp_generate2(
+            $(const char * secret), 
+            $(size_t secretLen),
+            $(time_t now),
+            $(unsigned int timeStepSize),
+            $(time_t startOffset),
+            $(unsigned int digits),
+            $(int flags),
+            $(char * outputOtp)
+        ) 
+    }|]
